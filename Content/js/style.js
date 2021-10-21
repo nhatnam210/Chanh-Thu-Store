3a25392b483b68cbd3e10f9a1104e716224f8fbc
Trần  Nguyễn Nhật Nam <tnnhatnam210@gmail.com>
2021-10-21 20:56:18 +0700

    logic tăng giảm, vượt quá tồn kho, thông báo thêm giỏ hàng

--- Content/js/style.js
+++ Content/js/style.js
@@ -1,33 +1,116 @@
-﻿
+﻿//lấy element cha theo selector
 function getParent(element, selector) {
-	while (element.parentElement) {
-		if (element.parentElement.matches(selector))
-			return element.parentElement;
+    while (element.parentElement) {
+        if (element.parentElement.matches(selector))
+            return element.parentElement;
 		else
-			element = element.parentElement
+            element = element.parentElement
 	}
 }
 
+//hiển thị menu danh mục con hiện tại theo URL
 const x = location.href
 
 var DanhMucCon = document.querySelectorAll('.danh-muc-con')
 
-Array.from(DanhMucCon).forEach((item) => {
+if (DanhMucCon.length > 0) {
+    Array.from(DanhMucCon).forEach((item) => {
 
-	const y = item.dataset.code
-	const parent = getParent(item,'.danh-muc')
+        const y = item.dataset.code
+        const parent = getParent(item, '.danh-muc')
 
-	if (x.includes(y)) {
-		item.classList.add('active')
-		parent.classList.add('show')
-	} else {
-		item.classList.remove('active')
+        if (x.includes(y)) {
+            item.classList.add('active')
+            parent.classList.add('show')
+        } else {
+            item.classList.remove('active')
 
-	}
+        }
+    })
+
+    if (x.endsWith('hang') || x.endsWith('hang/')) {
+        const parent = getParent(DanhMucCon[0], '.danh-muc')
+        if (parent) {
+            DanhMucCon[0].classList.add('active')
+            parent.classList.add('show')
+        }
+    }
+
+}
+
+
+//Sự kiện tăng giảm số lượng sản phẩm giỏ hàng
+const inputQuantity = document.querySelectorAll('.c-input-text')
+
+Array.from(inputQuantity).forEach((item) => {
+    const btnDecrease = document.querySelector(`.quantity-box .decrease[data-id="${item.dataset.id}"]`)
+    const btnIncrease = document.querySelector(`.quantity-box .increase[data-id="${item.dataset.id}"]`)
+    const message = document.querySelector(`.quantity-box .message[data-id="${item.dataset.id}"]`)
+
+    const maxValue = parseInt(item.getAttribute('maxValue'));
+
+    visibilityBtn()
+
+    btnDecrease.addEventListener('click', () => upDownValue(-1));
+    btnIncrease.addEventListener('click', () => upDownValue(1));
+    item.addEventListener('change', () => visibilityBtn())
+
+    //Thay đổi số lượng, ẩn hiện nút khi click tăng giảm
+    function upDownValue(option) {
+        let itemValue = parseInt(item.value);
+        if (option == 1) {
+            item.value = itemValue + 1;
+            itemValue = parseInt(item.value);
+        }
+        if (option == -1) {
+            item.value = itemValue - 1;
+            itemValue = parseInt(item.value);
+        }
+        visibilityBtn()
+    }
+
+    //Ẩn hiện nút theo số lượng
+    function visibilityBtn() {
+        if (parseInt(item.value) <= 1 || !item.value) {
+            item.value = 1;
+            btnDecrease.classList.remove('show')
+            btnIncrease.classList.add('show')
+            message.textContent = ""
+        } else if (parseInt(item.value) >= maxValue) {
+            item.value = parseInt(maxValue);
+            MaxValueMess()
+            btnIncrease.classList.remove('show')
+            btnDecrease.classList.add('show')
+        } else {
+            btnDecrease.classList.add('show')
+            btnIncrease.classList.add('show')
+            message.textContent = ""
+        }
+    }
+
+    //Hiện thông báo quá số lượng
+    function MaxValueMess() {
+        message.style.visibility = "visible"
+        message.textContent = "Đã đạt giới hạn tồn kho"
+        setTimeout(() => {
+            message.style.visibility = "hidden"
+        }, 1000)
+    }
 })
 
-if (x.endsWith('hang') || x.endsWith('hang/')) {
-	const parent = getParent(DanhMucCon[0], '.danh-muc')
-	DanhMucCon[0].classList.add('active')
-	parent.classList.add('show')
-}
+//Hiển thị thông báo thêm vào giỏ hàng thành công
+const listBtnShow = document.querySelectorAll('.add-cart-notify')
+const modal = document.querySelector('.modal')
+const notify = document.querySelector('.add-cart__success')
+
+Array.from(listBtnShow).forEach((item) => {
+    item.onclick = () => {
+        modal.classList.add('show')
+        notify.classList.add('show')
+
+        setTimeout(() => {
+            modal.classList.remove('show')
+            notify.classList.remove('show')
+        }, 2000)
+    }
+})
