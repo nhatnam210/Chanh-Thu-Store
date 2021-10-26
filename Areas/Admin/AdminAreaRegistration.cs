a34cbce812d691f7e5f7d5a6e15b02944dd93d7b
thuanphat2009 <anhxanh2019@gmail.com>
2021-10-26 21:53:03 +0700

    Danh muc , DMC , Hoa Don

--- Areas/Admin/AdminAreaRegistration.cs
+++ Areas/Admin/AdminAreaRegistration.cs
@@ -19,10 +19,25 @@ namespace ChanhThu_Store.Areas.Admin
                 "admin",
                 new { controller = "MainAdmin", action = "Index", id = UrlParameter.Optional }
             );
-        context.MapRoute(
-                "Nha san xuat admin",
-                "admin/nha-san-xuat",
-                new { controller = "NhaSanXuats", action = "Index", id = UrlParameter.Optional }
+            context.MapRoute(
+                    "Nha san xuat admin",
+                    "admin/nha-san-xuat",
+                    new { controller = "NhaSanXuats", action = "Index", id = UrlParameter.Optional }
+                );
+            context.MapRoute(
+                "Danh muc admin",
+                "admin/danh-muc",
+                new { controller = "DanhMucsAdmin", action = "Index", id = UrlParameter.Optional }
+            );
+            context.MapRoute(
+                "Danh muc con admin",
+                "admin/danh-muc-con",
+                new { controller = "DanhMucConsAdmin", action = "Index", id = UrlParameter.Optional }
+            );
+            context.MapRoute(
+                "Hoa Don admin",
+                "admin/hoa-don",
+                new { controller = "HoaDonsAdmin", action = "Index", id = UrlParameter.Optional }
             );
             context.MapRoute(
                 "Admin_default",
