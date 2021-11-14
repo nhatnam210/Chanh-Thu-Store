0854b95e2e6e5c001e7d3113e6b3f80f43074e3a
Trần  Nguyễn Nhật Nam <tnnhatnam210@gmail.com>
2021-11-14 17:08:49 +0700

    thông báo giỏ hàng trống, css lại in hóa đơn và 1 vài admin

---
+++ Areas/AssetAdmin/js/styleAdmin.js
@@ -0,0 +1,21 @@
+﻿/* ..............................................
+Active navbar-item admin theo URL & href
+................................................. */
+
+const linkNavbarMenuAdmin = document.querySelectorAll('.navbar-menu-admin .nav-item a')
+const locationHref = location.href
+
+if (linkNavbarMenuAdmin) {
+    if (locationHref.includes('danh-muc-con')) {
+        linkNavbarMenuAdmin[2].classList.add('active')
+    } else {
+        linkNavbarMenuAdmin.forEach((item) => {
+            if (locationHref.includes(item.href)) {
+                item.classList.add('active')
+            } else {
+                item.classList.remove('active')
+            }
+        })
+    }
+   
+}
