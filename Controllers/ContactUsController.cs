4e730765e535bbc62d32670f8204ed9cf7ab2db6
Trần  Nguyễn Nhật Nam <tnnhatnam210@gmail.com>
2021-12-06 21:20:32 +0700

    thêm partial view liên hệ, và in bill + loại sản phẩm trống notify

--- Controllers/ContactUsController.cs
+++ Controllers/ContactUsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChanhThu_Store.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,18 @@ namespace ChanhThu_Store.Controllers
 {
     public class ContactUsController : Controller
     {
+        private ChanhThuStoreContext db = new ChanhThuStoreContext();
         // GET: ContactUs
         public ActionResult Index()
         {
             return View();
         }
+
+        public PartialViewResult ThongTinLienHe()
+        {
+            var thongTinCuaHang = db.ThongTinCuaHangs.FirstOrDefault();
+            return PartialView(thongTinCuaHang);
+        }
+
     }
 }
